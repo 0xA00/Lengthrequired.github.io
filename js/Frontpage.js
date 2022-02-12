d812ae74e9bdd62410314f6750621c8ac6c700f1
Lengthrequired <hyp3rcr4ft@gmail.com>
2022-02-12 12:29:21 +0100

    site

--- js/Frontpage.js
+++ js/Frontpage.js
@@ -1,7 +1,7 @@
 var i =0;
 function alternate(){
   var doc = document.getElementById("whitespace");
-  var color = ["#2c2c2c","white"];
+  var color = ["#121C29","white"];
   doc.style.backgroundColor = color[i];
   doc.style.color=color[i];
   i = (i+1)%color.length;
